59f32184ccd4e0406b589923d22db7c489a03345
Bart van Oort <bart@vanoort.is>
2023-06-22 23:10:19 +0200

    Adds dietpi/build.sh script to use in arm-runner workflow

---
+++ dietpi/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+# This script is meant to be run inside a DietPi image.
+# It installs the necessary packages on top of DietPi to create the OS image for Pi-DJ.
+
+cat /boot/dietpi.txt
+
+# TODO: customize the image with the DietPi tools
+# TODO: create / customize the dietpi.txt file so that it boots nicely and it easily configurable.
