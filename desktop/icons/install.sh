8fa0e5a34bb7380af1ac4c7c4d38c280547e15a5
Bart van Oort <bart@vanoort.is>
2023-08-19 00:54:55 +0200

    Makes the icons install script able to update instead of just install

--- desktop/icons/install.sh
+++ desktop/icons/install.sh
@@ -1,2 +1,19 @@
-#!/bin/sh
-git clone https://github.com/EliverLara/candy-icons.git /usr/share/icons/candy-icons
+#!/bin/bash
+set -e
+
+# Install into system-wide icons directory if running as root, otherwise install into user's home directory
+icons_dir=/usr/share/icons
+if [[ "$(whoami)" != "root" ]]; then
+  icons_dir=/home/$(whoami)/.local/share/icons
+fi
+
+mkdir -p "$icons_dir"
+
+# If candy-icons has already been installed, just update, otherwise install by cloning.
+if [[ -e "$icons_dir/candy-icons" ]]; then
+  cd "$icons_dir/candy-icons"
+  git checkout master
+  git pull
+else
+  git clone --depth 1 https://github.com/EliverLara/candy-icons.git /usr/share/icons/candy-icons
+fi
